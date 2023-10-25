7332dfb13ea4d1d05816009c77a4e613358eb044
Piotr Poterek <peterpoterek0@gmail.com>
2023-10-25 22:18:21 +0200

    Task 9 - completed

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,15 @@
+const changeColorButton = document.querySelector(".change-color");
+const colorText = document.querySelector(".color");
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const handleColorChange = () => {
+  const currentColor = getRandomHexColor();
+  document.body.style.backgroundColor = currentColor;
+  colorText.innerHTML = currentColor;
+};
+
+changeColorButton.addEventListener("click", handleColorChange);
