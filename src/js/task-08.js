5eedf237f3d6b7a6ef6bc118b01b8dbfcd26d9d4
Piotr Poterek <peterpoterek0@gmail.com>
2023-10-25 21:44:39 +0200

    Task 8 - completed

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
 
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  const email = e.target.elements.email.value;
+  const password = e.target.elements.password.value;
+
+  if (email === "" || password === "") {
+    alert("Email and password fields are required");
+  } else {
+    const user = { email: email, password: password };
+    console.log(user);
+    form.reset();
+  }
+};
+form.addEventListener("submit", handleSubmit);
