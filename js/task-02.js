10181864c88385bf184ea29e4dbc2bec832a3c1d
Piotr Poterek <peterpoterek0@gmail.com>
2023-10-29 02:11:38 +0200

    Task 2 - adjusted

--- js/task-02.js
+++ js/task-02.js
@@ -1,11 +1,12 @@
 const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
 
 const list = document.querySelector("#ingredients");
-
+const liArr = [];
 ingredients.forEach((ingredient) => {
   const li = document.createElement("li");
   li.setAttribute("class", "item");
   li.innerHTML = ingredient;
-
-  list.append(li);
+  liArr.push(li);
 });
+
+list.append(...liArr);
--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,6 @@ const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
 
 const handleNameChange = (e) => {
-  console.log(e.target.value);
   if (e.target.value === "") {
     nameOutput.innerHTML = "Anonymous";
   } else {
